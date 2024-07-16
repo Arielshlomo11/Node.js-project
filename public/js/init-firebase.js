4056ebc728afcfb65186dd9dd3c1fddd56a34bb0
Arielshlomo11 <Arishlomo11@gmail.com>
2024-07-16 21:16:42 +0300

    Firebase Config Updated

--- public/js/init-firebase.js
+++ public/js/init-firebase.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyCEfBAF5mv80Aa1Nbc6Aeqvg50ftIOehw0",
-  authDomain: "node-project-59233.firebaseapp.com",
-  projectId: "node-project-59233",
-  storageBucket: "node-project-59233.appspot.com",
-  messagingSenderId: "592038020286",
-  appId: "1:592038020286:web:c0210bccf57ce50b978aef"
+  apiKey: "AIzaSyBq4jc8GhqwdVo5yrHVR6905gtV1yau82M",
+  authDomain: "nodejs-project-f53c8.firebaseapp.com",
+  projectId: "nodejs-project-f53c8",
+  storageBucket: "nodejs-project-f53c8.appspot.com",
+  messagingSenderId: "48662441639",
+  appId: "1:48662441639:web:7bcc37a3212970fe8a7582"
 };
 
 var firebaseApp = firebase.initializeApp(firebaseConfig);
